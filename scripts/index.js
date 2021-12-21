4f21f61a97df430d93d15dff0a0244402bdbb1c8
ilya120 <5374728@gmail.com>
2021-12-21 16:46:31 +0300

    "feat: доделанный popup"

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,35 @@
-let popup = document.querySelector('.popup'); // Объявляем переменную
-let editButton = document.querySelector('.profile__edit-button'); // Объявляем переменную
-let closeButton = document.querySelector('.popup__container-close-button'); // Объявляем переменную
-let editprofileTitle = document.querySelector('.popup__container-profile-title'); // Объявляем переменную
-let editprofileSubtitle = document.querySelector('.popup__container-profile-subtitle'); // Объявляем переменную
+let popup = document.querySelector('.popup'); // Объявляем переменные
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__container-close-button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__container-profile-title');
+let jobInput = document.querySelector('.popup__container-profile-subtitle');
 
 editButton.addEventListener('click',() => { // Вешаем обработчик на кнопку
   popup.classList.remove('popup_opened'); // Убираем активный класс
-  editprofileTitle.value = profileTitle.textContent; // "Исходный текст"
-  editprofileSubtitle.value = profileSubtitle.textContent; // "Исходный текст"
+  nameInput.value = profileTitle.textContent; // Исходный текст
+  jobInput.value = profileSubtitle.textContent; // Исходный текст
 });
 
 closeButton.addEventListener('click',() => { // Вешаем обработчик на кнопку
   popup.classList.add('popup_opened'); // Добавляем не активный класс
 });
 
+// Факультативно
 document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
   if(e.target === popup) { // Если цель клика - фон, то:
     popup.classList.add('popup_opened'); // Добавляем не активный класс
   }
 });
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__container');
-let submitButton = document.querySelector('.popup__container-submit-button');
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  profileTitle.textContent = nameInput.value; // Записываем введенный текст
+  profileSubtitle.textContent = jobInput.value; // Записываем введенный текст
 }
-
-submitButton.addEventListener('click',() => { // Вешаем обработчик на кнопку
   formElement.addEventListener('submit', formSubmitHandler);
-  popup.classList.add('popup_opened');
-  profileTitle.textContent = editprofileTitle.value; // "Записываем введенный текст"
-  profileSubtitle.textContent = editprofileSubtitle.value; // "Записываем введенный текст"
-});
+
